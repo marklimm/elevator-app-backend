bc823a4af7317d319ec9affa8f400d4f76585174
Mark <mark.limm@gmail.com>
2021-06-26 08:40:38 -0400

    start of personSpawnLoop and broadcasterLoop

--- lib/GameLoops.ts
+++ lib/GameLoops.ts
@@ -1,10 +1,10 @@
-import { Server } from 'socket.io'
+import { Server as SocketIOServer } from 'socket.io'
 
-import { building } from './Building'
+import { addPeople, getBroadcasterInformation, getNumUsers, spawnNewUser } from './BuildingState'
 import { getChangeByAmount } from './Randomizer'
 
 export class GameLoops {
-  private _io: Server
+  private _io: SocketIOServer
 
   /**
    * This flag is true when the game loops are currently running
@@ -16,12 +16,12 @@ export class GameLoops {
  */
    private intervalsArr: NodeJS.Timeout[] = []
 
-   constructor (io: Server) {
+   constructor (io: SocketIOServer) {
      this._io = io
      this._active = false
    }
 
-   public startGameLoops () : void {
+   public start () : void {
      this._active = true
 
      //  fluctating number of people in the building
@@ -29,38 +29,59 @@ export class GameLoops {
 
      //  "main" loop ... wondering if this will end up changing
      this.intervalsArr.push(setInterval(this.mainLoop.bind(this), 2500))
+
+     //  elevator manager
+     // this.intervalsArr.push(setInterval(getElevatorManagerLoop(this._io), 2500))
+
+     this.intervalsArr.push(setInterval(this.personSpawnLoop.bind(this), 3000))
+
+     this.intervalsArr.push(setInterval(this.broadcasterLoop.bind(this), 3000))
    }
 
-   public stopGameLoops () : void {
+   public stop () : void {
      this._active = false
 
      //  clear all of the intervals/game loops
      this.intervalsArr.forEach(interval => clearInterval(interval))
    }
 
-   public areRunning () : boolean {
+   get areRunning () : boolean {
      return this._active
    }
 
    private mainLoop () {
-     console.log(`-- loop is executing at ${new Date()}`)
+     //  console.log(`-- loop is executing at ${new Date()}`)
 
-     const numPeople = building.getNumPeople()
+     //  const numPeople = building.getNumPeople()
 
-     console.log(`there are currently ${numPeople} people in the building`)
+     //  console.log(`there are currently ${numPeople} people in the building`)
 
-     const newStatusMessage = {
-       name: 'some name',
-       text: `there are currently ${numPeople} people in the building`,
-       numPeopleInBuilding: numPeople
-     }
+     //  const newStatusMessage = {
+     //    name: 'some name',
+     //    text: `there are currently ${numPeople} people in the building`,
+     //    numPeopleInBuilding: numPeople
+     //  }
 
-     //  broadcast the number of people in the building to all clients
-     this._io.emit('status-update', newStatusMessage)
+     //  //  broadcast the number of people in the building to all clients
+     //  this._io.emit('status-update', newStatusMessage)
    }
 
    private fluctuatingNumPeopleLoop () : void {
      //  increase or decrease the amount of people in the building to simulate people entering and leaving the building
-     building.addPeople(getChangeByAmount())
+     addPeople(getChangeByAmount())
+   }
+
+   private personSpawnLoop () : void {
+     //  don't add more people if there's already one user
+     if (getNumUsers() > 0) { return }
+
+     spawnNewUser()
+   }
+
+   private broadcasterLoop () : void {
+     const broadcastStr = getBroadcasterInformation()
+
+     //  broadcast the number of people in the building to all clients
+     this._io.emit('status-update', { text: broadcastStr })
    }
 }
