ba5988e8b0192bf4beba08f804a3e7337a4d5960
Mark <mark.limm@gmail.com>
2021-06-26 22:32:54 -0400

    store game loops in a key-value object instead of an array

--- gameLoops/GameLoopManager.ts
+++ gameLoops/GameLoopManager.ts
@@ -4,7 +4,7 @@ import { Server as SocketIOServer } from 'socket.io'
 import { fluctuatingNumPeopleLoop } from './fluctuatingNumPeopleLoop'
 import { elevatorLoop } from './elevatorLoop'
 import { spawnNewUserLoop } from './spawnNewUserLoop'
-import { personLoop } from './personLoop'
+import { GameLoopIntervals } from '../lib/BuildingActions'
 import { getElevatorsAsArray } from '../state/Elevators'
 import { elevatorManagerLoop } from './elevatorManagerLoop'
 
@@ -19,47 +19,54 @@ export class GameLoopManager {
   /**
  * Contains the ids for all of the currently running game loops
  */
-   private intervalsArr: NodeJS.Timeout[] = []
+  //  private intervalsArr: NodeJS.Timeout[] = []
+  private intervalsObj: GameLoopIntervals = {}
 
-   constructor (io: SocketIOServer) {
-     this._io = io
-     this._active = false
-   }
+  constructor (io: SocketIOServer) {
+    this._io = io
+    this._active = false
+  }
 
-   public start () : void {
-     this._active = true
+  public start () : void {
+    this._active = true
 
-     //  fluctating number of people in the building
-     this.intervalsArr.push(setInterval(fluctuatingNumPeopleLoop.bind(this), 3000))
+    //  fluctating number of people in the building
+    //  this.intervalsArr.push()
 
-     //  "main" loop ... wondering if this will end up changing
-     //  this.intervalsArr.push(setInterval(this.mainLoop.bind(this), 2500))
+    this.intervalsObj['fluctuating-people'] = setInterval(fluctuatingNumPeopleLoop.bind(this), 3000)
 
-     //  elevator manager
-     // this.intervalsArr.push(setInterval(getElevatorManagerLoop(this._io), 2500))
+    //  "main" loop ... wondering if this will end up changing
+    //  this.intervalsArr.push(setInterval(this.mainLoop.bind(this), 2500))
 
-     this.intervalsArr.push(setInterval(spawnNewUserLoop.bind(this), 2000))
+    //  elevator manager
+    // this.intervalsArr.push(setInterval(getElevatorManagerLoop(this._io), 2500))
 
-     this.intervalsArr.push(setInterval(personLoop.bind(this), 5000))
+    //  this.intervalsArr.push()
+    this.intervalsObj['spawn-new-user-loop'] = setInterval(spawnNewUserLoop.bind(this, this.intervalsObj), 3000)
 
-     getElevatorsAsArray().forEach(elevator => {
-       this.intervalsArr.push(setInterval(elevatorLoop.bind(this, elevator), 1000))
-     })
+    //  this.intervalsArr.push(setInterval(personLoop.bind(this), 5000))
 
-     this.intervalsArr.push(setInterval(elevatorManagerLoop.bind(this), 4000))
+    getElevatorsAsArray().forEach(elevator => {
+      //  this.intervalsArr.push(setInterval(elevatorLoop.bind(this, elevator), 1000))
+      this.intervalsObj[`${elevator.name}`] = setInterval(elevatorLoop.bind(this, elevator), 1000)
+    })
 
-     //  reporting on any status changes that were caused by the above loops
-     //  this.intervalsArr.push(setInterval(statusLoop.bind(this), 2000))
-   }
+    //  this.intervalsArr.push(setInterval(elevatorManagerLoop.bind(this), 4000))
+    this.intervalsObj['elevator-manager'] = setInterval(elevatorManagerLoop.bind(this), 4000)
 
-   public stop () : void {
-     this._active = false
+    //  reporting on any status changes that were caused by the above loops
+    //  this.intervalsArr.push(setInterval(statusLoop.bind(this), 2000))
+  }
 
-     //  clear all of the intervals/game loops
-     this.intervalsArr.forEach(interval => clearInterval(interval))
-   }
+  public stop () : void {
+    this._active = false
 
-   get areRunning () : boolean {
-     return this._active
-   }
+    //  clear all of the intervals/game loops
+    Object.keys(this.intervalsObj).map(intervalKey => clearInterval(this.intervalsObj[intervalKey]))
+    // this.intervalsArr.forEach(interval => clearInterval(interval))
+  }
+
+  get areRunning () : boolean {
+    return this._active
+  }
 }
--- gameLoops/spawnNewUserLoop.ts
+++ gameLoops/spawnNewUserLoop.ts
@@ -1,12 +1,20 @@
+import { GameLoopIntervals } from '../lib/BuildingActions'
 import { broadcastUserStatusUpdate } from '../state/Broadcaster'
 import { getNumUsers, spawnNewUser } from '../state/People'
+import { personLoop } from './personLoop'
 
-export const spawnNewUserLoop = async () : Promise<void> => {
+export const spawnNewUserLoop = async (intervalsObj: GameLoopIntervals) : Promise<void> => {
   //  don't add more people if there's already one user
-  if (getNumUsers() > 0) { return }
+  if (getNumUsers() > 1) { return }
 
   const newUser = await spawnNewUser()
 
+  console.log(`${newUser} was just spawned`)
+  // intervalsArr.push(setInterval(personLoop.bind(null, newUser.name), 5000))
+  // setInterval(elevatorManagerLoop.bind(this), 4000)
+
+  intervalsObj[`${newUser.name}`] = setInterval(personLoop.bind(null, newUser.name), 5000)
+
   //  broadcast that a new user has been created
-  broadcastUserStatusUpdate([newUser])
+  broadcastUserStatusUpdate(newUser)
 }
